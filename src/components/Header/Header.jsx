59256173589d498da44bfb86b61d1ac24c34dbea
Talita Lima <talita.lima.tech@gmail.com>
2026-01-30 14:14:24 -0300

    feat: cria estrutura inicial do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-
-export default function Header() {
+const Header = () => {
   return (
     <header>
-      <h2>Header</h2>
+      <div>
+        <h1>LOGO</h1>
+      </div>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/products">Produtos</a>
+        <a href="/categories">Categorias</a>
+        <a href="/orders">Meus Pedidos</a>
+      </nav>
     </header>
   );
-}
+};
+
+export default Header;
