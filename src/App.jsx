6ef48372e457cc551cdcaded2e2fa02c5413607f
Talita Lima <talita.lima.tech@gmail.com>
2026-01-31 10:29:24 -0300

    feat(footer): commit inicial do footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Footer />
     
     </BrowserRouter>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import logoFooter from '../../assets/logo-footer.svg';
 
-export default function Footer() {
-  return (
-    <footer>
-      <h2>Footer</h2>
+const Footer = () => {
+  return ( 
+    <footer className="text-white p-4"
+      style={{ backgroundColor: "#1F1F1F" }}>
+      <img src={logoFooter} alt="Logo Digital Store" />
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nulla facilisi. Praesent vel dolor ut libero posuere gravida.
+      </p>
     </footer>
   );
 }
+ 
+export default Footer;
