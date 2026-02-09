6b7629f688f7f28bd7a5e3b01e0554add3741b2b
Talita Lima <talita.lima.tech@gmail.com>
2026-02-09 20:43:37 -0300

    feat: cria componente Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import 'primeflex/primeflex.css';
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
+import Section from './components/Section/Section';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+
+      <Section title="Destaques">
+        <p>A</p>
+      </Section>
+
+      <Section title="Novidades">
+        <p>B</p>
+      </Section>
       <Footer />
     </BrowserRouter>
   );
